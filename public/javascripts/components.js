112db0692b15d11d155391666a187c68516e9e16
Jason McLeod <pixelparty@gmail.com>
2011-08-31 12:29:37 -0400

    improved keyboard input, tap-to-move

--- public/javascripts/components.js
+++ public/javascripts/components.js
@@ -3,25 +3,24 @@ function loadComponents() {
     Crafty.c("gridMovement", {
         gridMovement: function(keys, interval, movement) {
             var self = this
-            var checkMovement = function() {
-                if(!self.attr('locked')) {
-                    if(self.isDown(Crafty.keys[keys.down]) || self.isDown(Crafty.keys[keys.down_secondary])) {
-                        socket.emit('move',{y:1})
-                    }
-                    if(self.isDown(Crafty.keys[keys.left]) || self.isDown(Crafty.keys[keys.left_secondary])) {
-                        socket.emit('move',{x:-1})
-                    }
-                    if(self.isDown(Crafty.keys[keys.right]) || self.isDown(Crafty.keys[keys.right_secondary])) {
-                        socket.emit('move',{x:1})
-                    }
+            var checkMovement = function(e) {
+                if(typeof e == 'undefined') e = { keyCode: 999 }
+                if(e.keyCode == keys.down || e.keyCode == keys.down_secondary || self.isDown(Crafty.keys[keys.down]) || self.isDown(Crafty.keys[keys.down_secondary])) {
+                    socket.emit('move',{y:1})
+                }
+                if(e.keyCode == keys.left || e.keyCode == keys.left_secondary || self.isDown(Crafty.keys[keys.left]) || self.isDown(Crafty.keys[keys.left_secondary])) {
+                    socket.emit('move',{x:-1})
+                }
+                if(e.keyCode == keys.right || e.keyCode == keys.right_secondary || self.isDown(Crafty.keys[keys.right]) || self.isDown(Crafty.keys[keys.right_secondary])) {
+                    socket.emit('move',{x:1})
                 }
             }
 
             this.gridMovementInterval = setInterval(checkMovement,interval,self)
 
-            this.bind('KeyPress', function(e) {
+            this.bind('KeyDown', function(e) {
                 clearInterval(this.gridMovementInterval)
-                checkMovement()
+                checkMovement(e)
                 this.gridMovementInterval = setInterval(checkMovement,interval,self)
             });
 
